bf3942d10b9a9428ed8f6006f04fd75d9b38ae6a
leonardodiasc <leonardodiass@gmail.com>
2022-05-19 14:51:46 -0300

    Now passwords are being encrypted and a hash protects the password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var userSchema = new mongoose.Schema({
+var bcrypt = require('bcrypt');
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
@@ -21,6 +22,18 @@ var userSchema = new mongoose.Schema({
     required: true
   }
 });
+// hash password before saving to the database
 
-var User = mongoose.model('User', userSchema);
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if(err){
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
