a97eba8811c1c27e5e2b702e0eb674127a11f529
leona <leona@LAPTOP-IG339IBJ>
2022-05-19 10:47:07 -0300

    creating an authentication website with encryption

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+// GET / register
+router.get('/register', (req,res,next) => {
+  return res.render('register', { title: 'Sign up'});
+});
+
+//POST/ Register
+router.post('/register', (req,res,next)=>{
+  return res.send('User created');
+});
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
